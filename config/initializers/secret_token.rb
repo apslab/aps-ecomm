fed25d2d27764e4ca9a0cb2fbcc69cdcff243f6f
luis petek <lmpetek@gmail.com>
2011-11-03 09:46:22 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecomm::Application.config.secret_token = 'ba6841035b0c82f27e27977067e81f66d537f6f0181a7bf1c920e8bdade77756158c1c9cde8ff750bb6d443ce01e7873af0a127100981b90631ab2df26365fca'
